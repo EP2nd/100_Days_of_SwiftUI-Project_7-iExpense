d30267332bc2373a4c1d2e31be78c9575fffc241
Edwin Przeźwiecki Jr <edwin.przezwiecki.jr@icloud.com>
2023-01-09 17:46:33 +0100

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Edwin Przeźwiecki Jr. on 09/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
